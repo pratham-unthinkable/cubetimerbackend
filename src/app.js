0cbae26ec28564aad857d24678becb527d48c652
pratham singh <pratham.singh@unthinkable.co>
2025-08-10 01:24:30 +0530

    added

--- src/app.js
+++ src/app.js
@@ -4,12 +4,16 @@ import logger from "./utils/logger.js";
 import { loadRoutes } from "./utils/loadRoutes.js";
 import errorMiddleware from "./middlewares/errorMiddleware.js";
 import { initApp } from "./utils/initApp.js";
+import cors from 'cors'
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
-
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"]
+}));
 app.use(express.json());
 
 app.use("/api/v1", loadRoutes());
